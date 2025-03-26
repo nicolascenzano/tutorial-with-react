065af1b4806a7251f9f65ac891fb7a826f3970e3
Nicolás Cenzano <nicolas.cenzano@uai.cl>
2025-03-26 10:14:45 -0300

    Agregar nueva página de ejemplo TICS420

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Index from "./pages/index";
 import Dinosaur from "./pages/Dinosaur";
+import PrograPRO from "./pages/tics420.tsx"
 import "./App.css";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Index />} />
+        <Route path="/tics420" element={<PrograPRO />} />
         <Route path="/:selectedDinosaur" element={<Dinosaur />} />
       </Routes>
     </BrowserRouter>
---
+++ src/pages/tics420.tsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+export default function PrograPRO() {
+
+  return (
+    <div>
+      <h1>Programación Profesional</h1>
+      <h3>TICS420</h3>
+      <p className="num-clase">Clase 03</p>
+      <Link to="/">Back to all dinosaurs</Link>
+    </div>
+  );
+}
